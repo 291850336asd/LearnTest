d17cf45b1cf9941ed061a11346d568be567894de
mengmeng.sun <mengmeng.sun@mtime.com>
2019-12-19 01:31:06 +0800

    修改dubbo 注册中心为redis

---
+++ distributed-teach/dubbo-easy-demo/src/main/java/com/meng/example/Test.java
@@ -0,0 +1,8 @@
+package com.meng.example;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("E304000000214194269".hashCode()%8);
+        System.out.println("E004000000214194195".hashCode()%8);
+    }
+}
