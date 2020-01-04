d2e6e0083fddb9444e4afb482aa6256dac4577cf
mengmeng.sun <mengmeng.sun@mtime.com>
2020-01-04 22:00:56 +0800

    netty demo

---
+++ distributed-teach/netty-learn/src/main/java/com/meng/example/demo/nettysolution/ServerMessageHandler.java
@@ -0,0 +1,27 @@
+package com.meng.example.demo.nettysolution;
+
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ExceptionEvent;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+/**
+ * 服务端消息处理类
+ */
+public class ServerMessageHandler extends SimpleChannelHandler {
+	
+	/**
+	 * 接收消息
+	 */
+	@Override
+	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+		System.out.println("receive request: " + e.getMessage());
+	}
+	
+	/**
+	 * 异常处理
+	 */
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+	}
+}
+
---
+++ distributed-teach/netty-learn/src/main/java/com/meng/example/demo/socket/ServerHandler.java
@@ -0,0 +1,15 @@
+package com.meng.example.demo.socket;
+
+import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.MessageEvent;
+import org.jboss.netty.channel.SimpleChannelHandler;
+
+public class ServerHandler extends SimpleChannelHandler{
+	
+	@Override
+	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+//		ChannelBuffer buffer = (ChannelBuffer) e.getMessage();
+//		byte[] bs = buffer.array();
+		System.out.println("server receive data: " + e.getMessage());
+	}
+}
