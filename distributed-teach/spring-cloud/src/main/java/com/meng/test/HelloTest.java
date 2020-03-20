e1a63d1ef1e0f575d41e98394a5ccdc42743c215
mengmeng.sun <mengmeng.sun@mtime.com>
2020-03-21 01:18:32 +0800

    spring cloud doc

--- distributed-teach/spring-cloud/src/main/java/com/meng/test/HelloTest.java
+++ distributed-teach/spring-cloud/src/main/java/com/meng/test/HelloTest.java
@@ -1,10 +1,21 @@
 package com.meng.test;
 
 
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @EnableEurekaClient
 public class HelloTest {
+
+
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+
 
     public static void main(String[] args) {
 
